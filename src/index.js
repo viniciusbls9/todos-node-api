4417d770661df2ba7f786d5ad1cede4048b2fcb9
Vinicius Benedito <beneditoloura@gmail.com>
2022-08-09 07:21:58 -0300

    ✨ feat: create checksExistsUserAccount middleware

--- src/index.js
+++ src/index.js
@@ -11,7 +11,15 @@ app.use(express.json());
 // const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const userExists = users.find(user => user.username === username)
+
+  if (!userExists) {
+    return response.status(400).json({ error: 'User not found' })
+  }
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
